c3d11ebc46727fe7b769891eb4680f5d5ae97c21
kdh990315 <104114383+kdh990315@users.noreply.github.com>
2024-09-09 04:45:45 +0900

    메인/슬라이드섹션

---
+++ src/js/utility/slideAnimation.js
@@ -0,0 +1,58 @@
+export class slideAnimation {
+	constructor(slideContainer, slideBtnContainers, slideWidth, slideMargin = 0) {
+		this.containers = document.querySelectorAll(slideContainer);
+		this.btnContainers = document.querySelectorAll(slideBtnContainers);
+		this.width = slideWidth;
+		this.margin = slideMargin;
+
+		this.slideInit();
+	}
+
+	slideInit() {
+		this.containers.forEach((container) => {
+			const slides = container.querySelectorAll('.slide');
+			container.style.width = `${(this.width + this.margin) * slides.length}px`;
+		});
+
+		this.slideEventListener();
+	}
+
+	slideEventListener() {
+		this.btnContainers.forEach((btnContainer, btnIdx) => {
+			btnContainer.addEventListener('click', (ev) => {
+				const target = ev.target;
+
+				if (!target.classList.contains('slide-btn')) return;
+
+				if (target.classList.contains('active')) return;
+
+				const btns = Array.from(btnContainer.querySelectorAll('.slide-btn'));
+				const btnIndex = btns.indexOf(target);
+				
+				this.textContainerClassHandler(btnContainer, btnIndex);
+
+				btns.forEach(item => {
+					item.classList.remove('active')
+				})
+
+				btns[btnIndex].classList.add('active');
+				
+				this.moveSlide(btnIdx, btnIndex);
+			});
+		});
+	}
+
+	textContainerClassHandler(btnContainer, btnIndex) {
+		const textContent = Array.from(btnContainer.querySelectorAll('.left-text'));
+
+		textContent.forEach((item) => {
+			item.classList.remove('active')
+		});
+
+		textContent[btnIndex].classList.add('active');
+	}
+
+	moveSlide(containerIdx, btnIndex) {		
+		this.containers[containerIdx].style.left = `${-this.width * btnIndex}px`;
+	};
+}
